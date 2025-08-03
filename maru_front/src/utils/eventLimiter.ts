34d9e4cf11836f91df0c0f29e7b4be602395b134
김승혁 <peter9889466@gmail.com>
2025-08-03 17:41:12 +0900

    feat : eventLimiter 주석 추가

--- maru_front/src/utils/eventLimiter.ts
+++ maru_front/src/utils/eventLimiter.ts
@@ -1,7 +1,10 @@
 import { useCallback, useRef } from 'react';
 
 /**
- * 디바운스 훅 - 마지막 호출 후 지정된 시간이 지나야 실행
+ * 디바운스 훅
+ * - 마지막 호출 후 지정된 시간이 지나야 실행
+ * - 사용자 입력 완료 후 처리 (예: 검색)
+ * - 검색 입력 멈춘 후 500ms 뒤 API 호출
  * @param callback 실행할 함수
  * @param delay 지연 시간 (ms)
  * @returns 디바운스된 함수
@@ -27,7 +30,10 @@ export const useDebounce = <T extends (...args: any[]) => any>(
 };
 
 /**
- * 스로틀링 훅 - 지정된 시간 간격으로만 실행
+ * 스로틀링 훅
+ * - 지정된 시간 간격으로만 실행
+ * - 고정된 간격으로 이벤트 처리 (예: 스크롤)
+ * - 스크롤 시 500ms마다 위치 업데이트
  * @param callback 실행할 함수
  * @param delay 간격 시간 (ms)
  * @returns 스로틀된 함수
