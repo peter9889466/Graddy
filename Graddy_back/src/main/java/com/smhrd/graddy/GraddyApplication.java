9fb9d42a80db0b26312f4a8c3df53744e247aefc
ASM <nano000417@naver.com>
2025-08-05 17:00:41 +0900

    back_end Spring 생성

---
+++ Graddy_back/src/main/java/com/smhrd/graddy/GraddyApplication.java
@@ -0,0 +1,15 @@
+package com.smhrd.graddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GraddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraddyApplication.class, args);
+	}
+
+}
---
+++ Graddy_back/src/test/java/com/smhrd/graddy/GraddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smhrd.graddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
