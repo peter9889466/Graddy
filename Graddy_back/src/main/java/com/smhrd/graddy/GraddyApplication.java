09019d66d42aca2c3d9457208a3bce3c8bb5fd8a
김승혁 <peter9889466@gmail.com>
2025-08-06 10:53:04 +0900

    test: test

--- Graddy_back/src/main/java/com/smhrd/graddy/GraddyApplication.java
+++ Graddy_back/src/main/java/com/smhrd/graddy/GraddyApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 
 public class GraddyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GraddyApplication.class, args);
+		// test
 	}
 
 }
