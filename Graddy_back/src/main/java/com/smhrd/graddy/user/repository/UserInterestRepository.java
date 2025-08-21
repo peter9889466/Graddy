e89bccb3240a41eff4a7682d940a70e4691ad730
JSW <tmddn1008@naver.com>
2025-08-21 12:08:09 +0900

    feat(user) : 마이페이지 아이디 변경 기능 추가 및 파일 수정

--- Graddy_back/src/main/java/com/smhrd/graddy/user/repository/UserInterestRepository.java
+++ Graddy_back/src/main/java/com/smhrd/graddy/user/repository/UserInterestRepository.java
@@ -2,8 +2,21 @@ package com.smhrd.graddy.user.repository;
 
 import com.smhrd.graddy.user.entity.UserInterest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserInterestRepository extends JpaRepository<UserInterest, UserInterest.UserInterestId> {
+    
+    // userId로 UserInterest 목록 조회
+    List<UserInterest> findByIdUserId(String userId);
+    
+    // userId로 UserInterest 삭제
+    @Modifying
+    @Query("DELETE FROM UserInterest ui WHERE ui.id.userId = :userId")
+    void deleteByIdUserId(@Param("userId") String userId);
 }
