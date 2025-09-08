2f7114fb32c6cf6d4b059e3d74c605b751886914
김승혁 <peter9889466@gmail.com>
2025-09-09 03:43:02 +0900

    1-4 스터디 커뮤니티 작성자 알수없음으로 뜨는것과 스터디원인데 댓글 작성 불가한거

--- Graddy_front/src/pages/Login.tsx
+++ Graddy_front/src/pages/Login.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
 import { User, Lock, AlertCircle, Check } from "lucide-react";
-import axios, { AxiosError } from "axios"; // AxiosError 타입을 가져옵니다.
+import axios, { AxiosError } from "axios";
 
 const Login: React.FC = () => {
     const [id, setId] = useState("");
@@ -64,7 +64,7 @@ const Login: React.FC = () => {
 
         try {
             const response = await axios.post(
-                "http://localhost:8080/api/auth/login",
+                "http://ec2-3-113-246-191.ap-northeast-1.compute.amazonaws.com/api/auth/login",
                 {
                     userId: id,
                     password: password,
